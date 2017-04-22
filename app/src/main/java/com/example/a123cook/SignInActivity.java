98fbe8c6ff2e6f2ece2a78e2e30c2c8c5cc68916
gn3544 <gnagar1996@utexas.edu>
2017-04-21 21:29:12 -0500

    My initial commit

--- app/src/main/java/com/example/a123cook/SignInActivity.java
+++ app/src/main/java/com/example/a123cook/SignInActivity.java
@@ -5,16 +5,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,13 +63,19 @@ import butterknife.ButterKnife;
         2. Add "forgot password" activity
 
  */
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
+
+
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @BindView(R.id.emailAddressInput) EditText emailAddressInput;
     @BindView(R.id.passwordInput) EditText passwordInput;
     @BindView(R.id.signInButton) Button signInButton;
     @BindView(R.id.signUpLink) TextView signUpLink;
 
+    private GoogleApiClient mGoogleApiClient;
     private FirebaseAuth auth;
     private ProgressDialog progressDialog;
 
@@ -47,6 +85,17 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signin);
         ButterKnife.bind(this);
 
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         auth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
 
@@ -55,36 +104,75 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
-    //----------onClick Methods-------------//
-    public void signIn() {
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        //Check user entry
-        if (!isValidEntry()){
-            onInvalidSignIn();
-            return;
-        }
-
-        String emailAddress = emailAddressInput.getText().toString();
-        String password = passwordInput.getText().toString();
-        progressDialog.setMessage("Signing in...");
-        progressDialog.show();
-
-        //Authorize with Firebase data store
-        auth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(this,
-                new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-                        if (task.isSuccessful()) {
-                            onValidSignIn();
-                        }
-                        else {
-                            onInvalidSignIn();
-                        }
-                    }
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            progressDialog.show();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    progressDialog.dismiss();
+                    handleSignInResult(googleSignInResult);
                 }
-        );
+            });
+        }
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
+    //----------onClick Methods-------------//
+//    public void signIn() {
+//
+//        //Check user entry
+//        if (!isValidEntry()){
+//            onInvalidSignIn();
+//            return;
+//        }
+//
+//        String emailAddress = emailAddressInput.getText().toString();
+//        String password = passwordInput.getText().toString();
+//        progressDialog.setMessage("Signing in...");
+//        progressDialog.show();
+//
+//        //Authorize with Firebase data store
+//        auth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(this,
+//                new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        progressDialog.dismiss();
+//                        if (task.isSuccessful()) {
+//                            onValidSignIn();
+//                        }
+//                        else {
+//                            onInvalidSignIn();
+//                        }
+//                    }
+//                }
+//        );
+//    }
 
     public void signUpRedirect() {
 
@@ -139,6 +227,36 @@ public class SignInActivity extends AppCompatActivity {
     public void onBackPressed() {
 
         moveTaskToBack(true);
+    }
+
+    //--------Sign-in helper methods--------//
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    public void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
 }
