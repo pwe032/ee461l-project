42bd6a0add61794411c24e231a6ee1d27a25c6a1
gn3544 <gnagar1996@utexas.edu>
2017-05-01 01:04:33 -0500

    I don't know anymore

--- app/src/main/java/com/example/a123cook/Recipe.java
+++ app/src/main/java/com/example/a123cook/Recipe.java
@@ -54,5 +54,4 @@ public class Recipe implements Serializable{
     }
 
 
-
 }
--- app/src/main/java/com/example/a123cook/RecipeDatabase.java
+++ app/src/main/java/com/example/a123cook/RecipeDatabase.java
@@ -9,15 +9,15 @@ public class RecipeDatabase {
     private static RecipeDatabase uniqueInstance;
     private static ArrayList<Recipe> allRecipes = new ArrayList<Recipe>();
 
-    private RecipeDatabase() {
+    public RecipeDatabase() {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference AllRecipeRef = database.getReference("allRecipes");
         DatabaseReference newRecipeRef = AllRecipeRef.push();
         newRecipeRef.setValue(new Recipe("Pizza", 1.0, "gn_logo", "Italian", "5", "instr", "ingredients"));
         AllRecipeRef.push().setValue(new Recipe("Sushi Rolls", 2.0, "mkp_logo", "Japanese", "5", "instr", "ingredients"));
-        AllRecipeRef.push().setValue(new Recipe("Crepe", 3.5, "mt_logo", "French", "5", "instr", "ingredients: n/a"));
-        AllRecipeRef.push().setValue(new Recipe("Baked Salmon", 0, "pb_logo", "American", "5", "1. prep ingredients\n2.cook food\n3.eat your food", "ingredients1, ingreidient 2, ingredient 3"));
+        AllRecipeRef.push().setValue(new Recipe("Crepe", 3.5, "mt_logo", "French", "5", "instr", "ingredients"));
+        AllRecipeRef.push().setValue(new Recipe("Baked Salmon", 0, "pb_logo", "American", "5", "1. prep ingredients\n2.cook food\n3.eat your food", "ingredients1 ingredient 2 ingredient3"));
     }
 
     public static RecipeDatabase getUniqueInstance() {
